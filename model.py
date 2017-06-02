fb6e3d3d92f26f81eefb731f442e2581cb5f1e01
soniccrhyme <victor.a.roy@gmail.com>
2017-06-02 04:01:01 -0400

    incorporate question freq

--- model.py
+++ model.py
@@ -13,6 +13,7 @@ import numpy as np
 import scipy as sp
 import pandas as pd
 from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
 from sklearn.metrics import log_loss
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
@@ -22,9 +23,10 @@ from fuzzywuzzy import fuzz
 from gensim import models
 import spacy # Spacy uses GloVe word embeddings
 
+train_size = 404290
+test_size = 2345796
 
-from nltk.corpus import stopwords
-from nltk.tokenize import RegexpTokenizer
+
 
 
 def load_data(test = False, extended = False):
@@ -70,21 +72,11 @@ def create_question_list(train, test):
     
     return questions_df
     
-# TODO: finish train-test split func
-def validation_split(df, y_name, test_size = 0.2):
-    
-    
-    y = df[y_name]
-    X = df.drop(y_name, axis = 1)
-    
-    
-    X_train, X_valid, y_train, y_valid = train_test_split(X,y, test_size = test_size)
-    
-    return X_train, X_valid, y_train, y_valid
-    
 
 def jaccard(str1, str2):
-    
+    '''
+    Return Jaccard score (|common|/|unique|) for questions
+    '''
     set1 = set(str1)
     set2 = set(str2)
     try:
@@ -96,6 +88,9 @@ def jaccard(str1, str2):
     
 
 def common_words(str1, str2):
+    '''
+    Return words common to both questions
+    '''
     
     set1 = set(str1)
     set2 = set(str2)
@@ -105,7 +100,9 @@ def common_words(str1, str2):
     return common
 
 def all_unique(str1, str2):
-    
+    '''
+    Return all unique words used by both questions
+    '''
     set1 = set(str1)
     set2 = set(str2)
 
@@ -222,6 +219,17 @@ def wm_distance(str1, str2, bed):
     
     return wm_dist
     
+# TODO: Finish normalize func
+def normalize(features, labels = None):
+    if labels == None:
+        labels = features.columns
+        
+    for key in labels:
+        features[key] = sp.stats.mstats.zscore(features[key])
+        
+    return features
+
+
 def extend_data(df, extended):
     '''
     Extended og dataframe by adding columns of various string subset tokenizations
@@ -280,7 +288,6 @@ def feature_gen(df, extended = None):
     t_1 = time.time()
     print('Jaccard-based features generated in {:.2f}s'.format(t_1-t_0))
     
-    '''
     embedding_name = 'glove.6B.50d.w2v.txt'
     if '.bin' in embedding_name:
         binary = True
@@ -289,13 +296,27 @@ def feature_gen(df, extended = None):
     word_vec = models.KeyedVectors.load_word2vec_format('embeddings/{}'.format(embedding_name), binary = binary)
     t_2 = time.time()
     print('Word embeddings loaded in {:.2f}s'.format(t_2-t_1))
-    '''
+    
     nlp = spacy.load('en')
     
     features['full_similarity_spacy'] = df.apply(lambda x: nlp(x['q1']).similarity(nlp(x['q2'])), axis = 1)
     features['unique_similarity_spacy'] = df.apply(lambda x: nlp(' '.join(sep_unique(x['q1_token'], x['q2_token']))).similarity(nlp(' '.join(sep_unique(x['q2_token'], x['q1_token'])))), axis = 1) 
     t_3 = time.time()
-    print('Similarity-based efatures generated in {:.2f}s'.format(t_3-t_1))
+    print('Spacy-based similarity features generated in {:.2f}s'.format(t_3-t_1))
+    features['full_wm_distance'] = df.apply(lambda x: wm_distance(x['q1'], x['q2'], word_vec), axis = 1)
+    t_4 = time.time()
+    print('Gensim-based word-mover distance generated in {:.2f}'.format(t_4-t_3))
+    
+    if features.shape[0] == train_size:
+        q_count = pd.read_csv('data/train_q_count.csv', index_col = 'id')
+    elif features.shape[0] == test_size:
+        q_count = pd.read_csv('data/test_q_count.csv', index_col = 'test_id')
+        
+    features['q1_freq'] = q_count['q1_count']
+    features['q2_freq'] = q_count['q2_count']
+    del q_count
+    t_5 = time.time()
+    print('Question frequency features added in {:.2f}s'.format(t_5-t_4))
     
     features['q1_word_count'] = df.apply(lambda x: len(x['q1_token']), axis = 1)
     features['q2_word_count'] = df.apply(lambda x: len(x['q2_token']), axis = 1)
@@ -306,6 +327,20 @@ def feature_gen(df, extended = None):
     print('All features generated in {:.2f}'.format(t_end-t_0))
     
     return features
+
+def get_feature_importance(clf = None, clf_name = None):
+    if clf:
+        return clf.booster().get_fscore()
+    elif clf_name:
+        if '.pickle' in clf_name:
+            pass
+        else:
+            clf_name += '.pickle'
+        with open('clfs/'+clf_name, 'rb') as f:
+            clf = pickle.load(f)
+        f.close()
+        return clf.booster().get_fscore()
+    
 
 def dump_classifier(clf, clf_name):
     '''
@@ -402,15 +437,29 @@ def xgb_clf(features, labels, clf_pickle = None):
     logloss_train = log_loss(y_train, y_train_hat)
     logloss_valid = log_loss(y_valid, y_valid_hat)
     
-    clf_name = 'SVC_{0}_{1:.3f}'.format(time.strftime('%m-%d-%H'), logloss_valid)+'.pickle'
+    clf_name = 'xgb_{0}_{1:.3f}'.format(time.strftime('%m-%d-%H'), logloss_valid)+'.pickle'
     dump_classifier(clf, clf_name)
     
     return clf, y_valid_hat, logloss_train, logloss_valid
 
+def create_prediction_file(features_test, clf, sub_name):
+    
+    predictions = pd.DataFrame(index = features_test.index)
+    predictions.index.name = 'test_id'
+    predictions['is_duplicate'] = clf.predict_proba(features_test)
+    
+    sub_name = sub_name
+    predictions.to_csv('submissions/{}.csv'.format(sub_name), )
+    
+    return
+
 def main():
+    # Mode defining parameters
     extended = True
     # TODO: Change load_test to testing_mode (inverse)
     load_test = False
+    
+    # Load data based on mode
     if load_test:
         train_df, test_df = load_data(test = load_test, extended = extended)
         print('Train & Test Loaded')
@@ -418,6 +467,7 @@ def main():
         train_df = load_data(test = load_test, extended = extended)
         print('Train Loaded')
         
+    # Generate features based on mode
     if extended == True:
         features = feature_gen(train_df, extended = None)
         if load_test:
@@ -427,10 +477,14 @@ def main():
         if load_test:
             features_test = feature_gen(test_df, extended = 'test_extended')
     
+    # TODO: Add queston frequency as feature - data leak
+    
+    # Save features to csv
     features.to_csv('data/features.csv')
     if load_test:
         features_test.to_csv('data/features_test.csv')
         
+    # Define labels
     labels = train_df['is_duplicate']
     
     assert features.shape[0] == labels.shape[0]
@@ -442,9 +496,12 @@ def main():
     print('Training Logloss: {}'.format(logloss_train))
     print("Validation Logloss: {}".format(logloss_valid))
     
+    feature_importance = get_feature_importance(clf = clf)
+    print(feature_importance)
+    
+    # Create prediction file
     if load_test:
-        # TODO: create prediction file
-        pass
+        create_prediction_file(features_test, clf)
     
     
 main()
